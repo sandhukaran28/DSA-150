30052536d0b04d7a9a64e7eb72e508f3fbb3c1d8
Karan Sandhu <sandhukaran2821@gmail.com>
2025-05-11 20:37:24 +1000

    Question Revesit Updated

--- Stacks/Car_Fleet.cs
+++ Stacks/Car_Fleet.cs
@@ -1,19 +1,18 @@
 public class Solution {
     public int CarFleet(int target, int[] position, int[] speed) {
         Array.Sort(position,speed);
-        Stack<float> st= new Stack<float>();
-
-        for(int i = position.Length -1 ;i >=0 ; i--){
+        Stack<float> st = new Stack<float>();
+        for(int i = position.Length - 1; i >= 0;i--){
             float time = ((float)target - position[i])/speed[i];
-            if(st.Count == 0){
-                st.Push(time);
-            }
-            else{
+            if(st.Count != 0){
                 if(time > st.Peek()){
                     st.Push(time);
-                }
+                } 
             }
-        } 
+            else{
+                st.Push(time);
+            }
+        }
 
         return st.Count;
     }
