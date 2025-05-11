accead043c86020e2e019f83e59d7b2b2b8c4473
Karan Sandhu <sandhukaran2821@gmail.com>
2025-05-11 21:26:47 +1000

    Question Revesit Updated

--- Binary Search/Search_2D_Array.cs
+++ Binary Search/Search_2D_Array.cs
@@ -1,26 +1,14 @@
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
-        int l = 0, r = matrix.Length - 1;
+        int rows = matrix.Length, cols = matrix[0].Length;
+        int l = 0 , r = (rows * cols) - 1;
         while(l <= r){
             int mid = (l+r)/2;
-            if(target >= matrix[mid][0] && target <= matrix[mid][matrix[mid].Length -1]){
-                int l_col = 0 , r_col  = matrix[mid].Length -1;
-                while(l_col <= r_col){
-                    int mid_col = (l_col + r_col)/2;
-                    if(target == matrix[mid][mid_col]){
-                        return true;
-                    }
-                    else if(target < matrix[mid][mid_col]){
-                        r_col = mid_col -1;
-                    }
-                    else{
-                        l_col = mid_col + 1;
-                    }
-                }
-                break;
+            if(matrix[mid/cols][mid % cols] == target){
+                return true;
             }
-            else if(target < matrix[mid][0]){
-                r = mid - 1;
+            else if( matrix[mid/cols][mid % cols] > target){
+                r = mid -1;
             }
             else{
                 l = mid +1;
