f1d80df9eb0cf2ba77a4e0788f894b84a3405885
Karan Sandhu <sandhukaran2821@gmail.com>
2025-05-11 21:42:47 +1000

    Question Revesit Updated

--- Binary Search/Koko_Eating_Bananas.cs
+++ Binary Search/Koko_Eating_Bananas.cs
@@ -1,29 +1,27 @@
 public class Solution {
     public int MinEatingSpeed(int[] piles, int h) {
         int max = -1;
-        for(int i = 0;i< piles.Length ;i++){
+        for(int i = 0;i< piles.Length;i++){
             if(piles[i] > max){
                 max = piles[i];
             }
         }
-
-        int l = 1, r = max , sol = r;
-        while(l <= r){
-            int mid = (l+r)/2;
-            int hours  = 0;
-             for(int i = 0;i< piles.Length ;i++){
-                hours+= (int)Math.Ceiling((double)piles[i] / mid);
-             }
-
-             if(hours <= h){
-                sol = mid;
-                r = mid -1;
-             }
-             else{
-                l = mid+1;
-             }
-        }
-
-        return sol;
+        int ans = max;
+        int l = 1, r = max;
+            while(l <= r){
+                int mid = (l+r)/2;
+                int count = 0;
+                for(int j = 0;j< piles.Length;j++){
+                    count += (int)Math.Ceiling((double)piles[j] / mid);
+                }
+                if(count > h){
+                    l = mid+1;
+                }
+                else{
+                    r = mid -1;
+                    ans = mid;
+                }
+            }
+            return ans;
     }
 }
