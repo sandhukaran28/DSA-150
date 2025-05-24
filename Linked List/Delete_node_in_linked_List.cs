109d73392291f14dbd81a55f859b7b4794e85641
Karan Sandhu <sandhukaran2821@gmail.com>
2025-05-24 11:16:29 +1000

    New Question Added

---
+++ Linked List/Delete_node_in_linked_List.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public void DeleteNode(ListNode node)
+    {
+        while (node != null && node.next != null)
+        {
+            node.val = node.next.val;
+            if (node.next.next == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+
+                node = node.next;
+            }
+
+        }
+    }
+}
