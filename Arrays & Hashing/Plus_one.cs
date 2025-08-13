feb48a90d2ddffe5afd0732e2c3fc1467bb7f303
Karan Sandhu <sandhukaran2821@gmail.com>
2025-08-14 09:35:45 +1000

    New Question Added

---
+++ Arrays & Hashing/Plus_one.cs
@@ -0,0 +1,36 @@
+public class Solution
+{
+    public int[] PlusOne(int[] arr)
+    {
+        int carry = 1;
+        int len = arr.Length;
+        for (int i = len - 1; i >= 0; i--)
+        {
+            arr[i] = arr[i] + carry;
+            carry = 0;
+            if (arr[i] > 9)
+            {
+                carry = arr[i] / 10;
+                arr[i] = arr[i] % 10;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (carry == 0)
+        {
+            return arr;
+        }
+
+        int[] res = new int[len + 1];
+        res[0] = carry;
+        for (int i = 1; i < len + 1; i++)
+        {
+            res[i] = arr[i - 1];
+        }
+
+        return res;
+    }
+}
