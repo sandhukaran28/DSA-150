844e557bd7829e782d722c73fc834be8c7452273
Karan Sandhu <sandhukaran2821@gmail.com>
2025-08-23 11:39:01 +1000

    New Question Added

--- Arrays & Hashing/Top_K_Frequent_Elements.cs
+++ Arrays & Hashing/Top_K_Frequent_Elements.cs
@@ -1,17 +1,52 @@
-public class Solution {
-    public int[] TopKFrequent(int[] nums, int k) {
-        Dictionary<int,int> dic = new Dictionary<int,int>();
-        for(int i=0;i<nums.Length;i++){
-            if(dic.ContainsKey(nums[i])){
-                dic[nums[i]] = ++dic[nums[i]];
+public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+
+        int n = nums.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!dic.ContainsKey(nums[i]))
+            {
+                dic[nums[i]] = 0;
             }
-            else{
-                dic[nums[i]] = 1;
+
+            dic[nums[i]]++;
+        }
+
+
+        var bucket = new List<int>[n + 1];
+
+        foreach (var kv in dic)
+        {
+            int val = kv.Value;
+            if (bucket[val] == null)
+            {
+                bucket[val] = new List<int>();
+            }
+
+            bucket[val].Add(kv.Key);
+        }
+
+        var res = new List<int>(k);
+
+        for (int i = n; i >= 1 && res.Count < k; i--)
+        {
+            if (bucket[i] != null)
+            {
+                foreach (var b in bucket[i])
+                {
+                    res.Add(b);
+                    if (res.Count == k)
+                    {
+                        break;
+                    }
+                }
             }
         }
 
-        List<int> ls = dic.OrderByDescending(pair => pair.Value).Select(pair => pair.Key).Take(k).ToList();
-
-        return ls.ToArray();
+        return res.ToArray();
     }
 }
