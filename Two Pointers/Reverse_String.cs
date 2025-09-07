345180b5916bbb0ed5a8b1a6ea582badc7f5bcff
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-07 11:20:17 +1000

    New Question Added

---
+++ Two Pointers/Reverse_String.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int l = 0, r = s.Length - 1;
+        while (l < r)
+        {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
