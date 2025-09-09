c6fa8b08d0cb247b6afd3a57a22faf5c2c91a9ca
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-10 09:25:08 +1000

    New Question Added

--- Sliding Window/Longest_Substring_Without_Repeating_Characters.cs
+++ Sliding Window/Longest_Substring_Without_Repeating_Characters.cs
@@ -1,16 +1,52 @@
-public class Solution {
-    public int LengthOfLongestSubstring(string s) {
-        Dictionary<char, int> dic = new Dictionary<char,int>();
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        Dictionary<char, int> dic = new Dictionary<char, int>();
         int l = 0, max = s.Length > 0 ? 1 : 0;
-        for(int i = 0;i <s.Length;i++){
-            if(dic.ContainsKey(s[i]) && dic[s[i]] >= l){
-                 l = dic[s[i]] + 1;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dic.ContainsKey(s[i]) && dic[s[i]] >= l)
+            {
+                l = dic[s[i]] + 1;
             }
 
             dic[s[i]] = i;
-            max = Math.Max(i-l + 1, max);
+            max = Math.Max(i - l + 1, max);
         }
 
         return max;
+    }
+}
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length == 0 || s.Length == 1)
+        {
+            return s.Length;
+        }
+        Dictionary<char, int> dic = new Dictionary<char, int>();
+        int l = 0, r = 0, ans = 0;
+        while (r < s.Length)
+        {
+            char ch = s[r];
+            if (dic.ContainsKey(ch))
+            {
+                int idx = dic[ch];
+                while (l <= idx)
+                {
+                    dic.Remove(ch);
+                    l++;
+                }
+            }
+            dic[ch] = r;
+            ans = Math.Max(ans, r - l + 1);
+
+            r++;
+        }
+
+        return ans;
     }
 }
