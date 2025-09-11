fe237559a164ab8b14fb30e5e2f5aea6d77b452a
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-12 09:46:41 +1000

    New Question Added

--- Stacks/Evaluate_Reverse_polish_notation.cs
+++ Stacks/Evaluate_Reverse_polish_notation.cs
@@ -1,27 +1,44 @@
-public class Solution {
-    public int EvalRPN(string[] arr) {
+public class Solution
+{
+    public int EvalRPN(string[] tokens)
+    {
         Stack<int> st = new Stack<int>();
-        for(int i = 0;i < arr.Length;i++){
-            if(arr[i] == "*" || arr[i] == "+" || arr[i] == "-" || arr[i] == "/"){
-                int n1 = st.Pop();
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            string curr = tokens[i];
+            if (curr.Length == 1 &&
+            (curr == "+" || curr == "-" || curr == "/" || curr == "*"))
+            {
                 int n2 = st.Pop();
-                switch(arr[i]){
+                int n1 = st.Pop();
+                switch (curr)
+                {
                     case "+":
-                        st.Push(n1 + n2);
-                        break;
-                         case "-":
-                        st.Push(n2 - n1);
-                        break;
-                         case "*":
-                        st.Push(n1 * n2);
-                        break;
-                         case "/":
-                        st.Push(n2 / n1);
-                        break;
+                        {
+                            st.Push(n1 + n2);
+                            break;
+                        }
+                    case "-":
+                        {
+                            st.Push(n1 - n2);
+                            break;
+                        }
+                    case "*":
+                        {
+                            st.Push(n1 * n2);
+                            break;
+
+                        }
+                    case "/":
+                        {
+                            st.Push(n1 / n2);
+                            break;
+                        }
                 }
             }
-            else{
-                st.Push(int.Parse(arr[i]));
+            else
+            {
+                st.Push(int.Parse(curr));
             }
         }
 
