b3fc9faf0fd202e24900590a9d838fe3378c355c
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-13 19:27:26 +1000

    New Question Added

--- Sliding Window/Longest_Substring_Without_Repeating_Characters.cs
+++ Sliding Window/Longest_Substring_Without_Repeating_Characters.cs
@@ -1,21 +1,20 @@
-public class Solution
-{
-    public int LengthOfLongestSubstring(string s)
-    {
-        Dictionary<char, int> dic = new Dictionary<char, int>();
-        int l = 0, max = s.Length > 0 ? 1 : 0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (dic.ContainsKey(s[i]) && dic[s[i]] >= l)
-            {
-                l = dic[s[i]] + 1;
-            }
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        Dictionary<char,int> dic = new Dictionary<char,int>();
+        int  l = 0, ans = 0;
+        for(int r = 0;r< s.Length;r++){
+            char ch = s[r];
+            if(dic.ContainsKey(ch)){
+                 if(dic[ch] >= l){
+                   l = dic[ch]+1;
+                }
 
-            dic[s[i]] = i;
-            max = Math.Max(i - l + 1, max);
+            }
+            dic[ch] = r;
+            ans = Math.Max(r-l+1, ans);
         }
 
-        return max;
+        return ans;
     }
 }
 
