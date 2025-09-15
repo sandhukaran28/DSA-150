3843d07592ceac90a95f432198896668fe34d633
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-16 08:48:35 +1000

    new question added

--- Binary Search/Binary_Search.cs
+++ Binary Search/Binary_Search.cs
@@ -1,18 +1,26 @@
-public class Solution {
-    public int Search(int[] nums, int target) {
-        int l = 0 ,r = nums.Length - 1;
-        while(l <= r){
-            int mid = (l+r)/2;
-            if(nums[mid] == target){
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length - 1;
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+
+            if (nums[mid] == target)
+            {
                 return mid;
             }
-            else if(target > nums[mid]){
+            else if (nums[mid] < target)
+            {
                 l = mid + 1;
             }
-            else{
+            else
+            {
                 r = mid - 1;
             }
         }
+
         return -1;
     }
 }
