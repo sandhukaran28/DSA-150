5b730c0286c2fc654c7280e7047982f6f1ba7b23
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-16 09:10:53 +1000

    new question added

--- Binary Search/Find_Minimum_In_Sorted_Array.cs
+++ Binary Search/Find_Minimum_In_Sorted_Array.cs
@@ -1,13 +1,19 @@
-public class Solution {
-    public int FindMin(int[] nums) {
-        int  l =0, r = nums.Length -1;
-        while(l < r){
-            int mid = (l+r)/2;
-            if(nums[r] < nums[mid]){
+public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        int l = 0, r = nums.Length - 1;
+
+        while (l < r)
+        {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[r])
+            {
                 l = mid + 1;
             }
-            else{
-            r = mid;
+            else
+            {
+                r = mid;
             }
         }
 
