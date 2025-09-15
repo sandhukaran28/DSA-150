8e742b9abd8d82c32df5064ae4698478d89cd0f4
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-16 08:58:22 +1000

    new question added

---
+++ Binary Search/Search_Insert_Position.cs
@@ -0,0 +1,26 @@
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length - 1;
+
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        return l;
+    }
+}
