870cbf7e1324e4f4ded859f833eb81a0bace668f
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-17 14:19:06 +1000

    new question added

--- Binary Search/Koko_Eating_Bananas.cs
+++ Binary Search/Koko_Eating_Bananas.cs
@@ -1,27 +1,46 @@
-public class Solution {
-    public int MinEatingSpeed(int[] piles, int h) {
-        int max = -1;
-        for(int i = 0;i< piles.Length;i++){
-            if(piles[i] > max){
-                max = piles[i];
+public class Solution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        int r = 0;
+        int n = piles.Length;
+        for (int i = 0; i < n; i++)
+        {
+            r = Math.Max(r, piles[i]);
+        }
+
+
+        int l = 1, ans = int.MaxValue;
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            if (isValid(piles, mid, h))
+            {
+                ans = mid;
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+
+        }
+
+        return ans;
+    }
+
+    public bool isValid(int[] piles, int k, int h)
+    {
+        for (int i = 0; i < piles.Length; i++)
+        {
+            double curr = Math.Ceiling((double)piles[i] / k);
+            h -= (int)curr;
+            if (h < 0)
+            {
+                break;
             }
         }
-        int ans = max;
-        int l = 1, r = max;
-            while(l <= r){
-                int mid = (l+r)/2;
-                int count = 0;
-                for(int j = 0;j< piles.Length;j++){
-                    count += (int)Math.Ceiling((double)piles[j] / mid);
-                }
-                if(count > h){
-                    l = mid+1;
-                }
-                else{
-                    r = mid -1;
-                    ans = mid;
-                }
-            }
-            return ans;
+
+        return h >= 0 ? true : false;
     }
 }
