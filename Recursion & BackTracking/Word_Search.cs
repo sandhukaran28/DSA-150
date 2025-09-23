4ddd7cf5532871b42ef259e0ae596b641eec140d
Karan Sandhu <sandhukaran2821@gmail.com>
2025-09-23 10:57:18 +1000

    new question added

--- Recursion & BackTracking/Word_Search.cs
+++ Recursion & BackTracking/Word_Search.cs
@@ -6,47 +6,42 @@ public class Solution
         {
             for (int j = 0; j < board[i].Length; j++)
             {
-                if (board[i][j] == word[0])
+                bool ans = sol(board, word, i, j, 0);
+                if (ans)
                 {
-                    bool res = sol(board, word, 0, i, j);
-                    if (res == true)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
         }
+
         return false;
     }
 
-    public bool sol(char[][] arr, string word, int index, int i, int j)
+    public bool sol(char[][] board, string word, int i, int j, int index)
     {
 
-        if (i >= arr.Length || j >= arr[0].Length || i < 0 || j < 0 || arr[i][j] == '#')
-        {
-            return false;
-        }
-
-        if (arr[i][j] == word[index])
-        {
-            index++;
-        }
-        else
-        {
-            return false;
-        }
         if (index == word.Length)
         {
             return true;
         }
-        char c = arr[i][j];
-        arr[i][j] = '#';
-        bool s1 = sol(arr, word, index, i + 1, j);
-        bool s2 = sol(arr, word, index, i - 1, j);
-        bool s3 = sol(arr, word, index, i, j + 1);
-        bool s4 = sol(arr, word, index, i, j - 1);
-        arr[i][j] = c;
-        return s1 || s2 || s3 || s4;
 
+        if (i < 0 || i >= board.Length || j < 0 || j >= board[0].Length)
+        {
+            return false;
+        }
+
+        if (board[i][j] == word[index])
+        {
+            board[i][j] = '.';
+            bool c1 = sol(board, word, i, j + 1, index + 1);
+            bool c2 = sol(board, word, i, j - 1, index + 1);
+            bool c3 = sol(board, word, i + 1, j, index + 1);
+            bool c4 = sol(board, word, i - 1, j, index + 1);
+            board[i][j] = word[index];
+
+            return c1 || c2 || c3 || c4;
+        }
+
+        return false;
     }
 }
