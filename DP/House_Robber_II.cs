88c25dd703203dd817896238680730713d00c6dc
Karan Sandhu <sandhukaran2821@gmail.com>
2025-11-08 11:29:04 +1000

    new question added

--- DP/House_Robber_II.cs
+++ DP/House_Robber_II.cs
@@ -1,34 +1,28 @@
-public class Solution
-{
-    public int Rob(int[] nums)
-    {
+public class Solution {
+    public int Rob(int[] nums) {
         int[] dp = new int[nums.Length];
-        Array.Fill(dp, -1);
-        int n1 = sol(nums, 0, 0, dp);
-        Array.Fill(dp, -1);
-        int n2 = sol(nums, 1, 1, dp);
-        return Math.Max(n1, n2);
+        Array.Fill(dp,-1);
+
+        int[] dp2 = new int[nums.Length];
+        Array.Fill(dp2,-1);
+
+        return Math.Max(sol(nums,2,true,dp)+nums[0],sol(nums,1,false,dp2)); 
     }
 
-    public int sol(int[] nums, int index, int start, int[] dp)
-    {
-        if (index >= nums.Length)
-        {
-            return 0;
-        }
-        if (index == nums.Length - 1 && nums.Length > 1 && start == 0)
-        {
+    public int sol(int[] nums,int idx, bool first,int[] dp){
+
+        if(idx >= nums.Length){
             return 0;
         }
 
-        if (dp[index] != -1)
-        {
-            return dp[index];
+        if(idx == nums.Length -1 && first == true){
+            return 0;
         }
 
-        int n1 = nums[index] + sol(nums, index + 2, start, dp);
-        int n2 = sol(nums, index + 1, start, dp);
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
 
-        return dp[index] = Math.Max(n1, n2);
+        return dp[idx] = Math.Max((sol(nums,idx+2,first,dp)+nums[idx]),sol(nums,idx+1,first,dp));
     }
 }
