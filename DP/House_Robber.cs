4c77af7be80ab0574c03fff05fdbbf6f552896f3
Karan Sandhu <sandhukaran2821@gmail.com>
2025-11-08 11:03:18 +1000

    new question added

--- DP/House_Robber.cs
+++ DP/House_Robber.cs
@@ -1,29 +1,27 @@
-public class Solution
-{
-    public int Rob(int[] nums)
-    {
+public class Solution {
+    public int Rob(int[] nums) {
         int[] dp = new int[nums.Length];
-        for (int i = 0; i < nums.Length; i++)
-        {
-            dp[i] = -1;
-        }
-
-        return sol(nums, 0, dp);
+        Array.Fill(dp,-1);
+        return sol(nums,0,dp);
     }
 
-    public int sol(int[] nums, int index, int[] dp)
-    {
-        if (index >= nums.Length)
-        {
+    public int sol(int[] nums, int idx,int[] dp){
+
+        if(idx >= nums.Length){
             return 0;
         }
-        if (dp[index] != -1)
-        {
-            return dp[index];
-        }
-        int n1 = nums[index] + sol(nums, index + 2, dp);
-        int n2 = sol(nums, index + 1, dp);
 
-        return dp[index] = Math.Max(n1, n2);
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+
+        //rob
+        int c1 = sol(nums,idx+2,dp) + nums[idx];
+
+        //don't rob
+        int c2  = sol(nums,idx+1,dp);
+
+        return dp[idx] = Math.Max(c1,c2);
+
     }
 }
